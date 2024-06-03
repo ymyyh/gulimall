3b3f89af504294198a17fa2f70686792a57bf116
hnzheng <865895777@qq.com>
2024-06-04 01:33:40 +0800

    项目创建

---
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.product.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
